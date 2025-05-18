d8b3e4790052886b3efc3b33d51b07ba286c4c93
DzeBakuEso <dzeblebaku@gmail.com>
2025-05-18 08:52:29 -1200

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,7 @@ class TestMemoize(unittest.TestCase):
     """Unit tests for memoize decorator"""
 
     def test_memoize(self):
-        """Test that a memoized function is only called once"""
+        """Test that memoization calls the method only once"""
 
         class TestClass:
             def a_method(self):
@@ -20,13 +20,9 @@ class TestMemoize(unittest.TestCase):
         with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
             test_obj = TestClass()
 
-            # Access property twice
             result1 = test_obj.a_property
             result2 = test_obj.a_property
 
-            # Assert values are correct
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
-
-            # Assert method was called only once due to memoization
             mock_method.assert_called_once()
